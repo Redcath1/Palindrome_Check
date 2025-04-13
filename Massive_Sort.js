d9a91c77f790f1db34ccaa2a6e52216d4c706584
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-13 21:36:25 +0500

    Add files via upload

---
+++ Massive_Sort.js
@@ -0,0 +1,22 @@
+const students = [
+  { id: 1, name: "John", mark: 5 },
+  { id: 2, name: "Peter", mark: 4 },
+  { id: 3, name: "Vincent", mark: 3 },
+  { id: 4, name: "Sebastien", mark: 5 },
+  { id: 5, name: "Alex", mark: 2 },
+];
+const poorsQuantity = students.filter((students) => students.mark === 2).length;
+const fivers = students.some(students => students.mark === 5);
+const massiveMarks = students.map(students => students.mark);
+let sum = 0
+for (let i = 0; i < massiveMarks.length; i++) {
+  sum += massiveMarks[i];
+}
+const averageMark = sum / massiveMarks.length;
+
+console.log("количество двоечников", poorsQuantity);
+console.log("есть ли отличники", fivers);
+console.log('средняя оценка', averageMark)
+
+
+//Сколько двоечников? Есть ли отличники/двоечники? Средний балл?
