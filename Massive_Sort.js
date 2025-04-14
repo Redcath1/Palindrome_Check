d88ecaf68e38371c92684c0c0d197f1cd80c3f76
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-14 18:52:48 +0500

    added marksArray
    ololo

--- Massive_Sort.js
+++ Massive_Sort.js
@@ -7,15 +7,16 @@ const students = [
 ];
 const poorsQuantity = students.filter((students) => students.mark === 2).length;
 const fivers = students.some((students) => students.mark === 5);
-const marksArray = students.map((students) => students.mark);
+const massiveMarks = students.map((students) => students.mark);
 const sum = students.reduce(
   (accumulator, currentValue) => accumulator + currentValue.mark,
   0
 );
-const averageMark = sum / marksArray.length;
-const isEver = marksArray.every((student) => students.mark === 2);
+const averageMark = sum / massiveMarks.length;
+const isEver = massiveMarks.every((student) => students.mark === 2);
 console.log("количество двоечников", poorsQuantity);
 console.log("есть ли отличники", fivers);
 console.log("средняя оценка", averageMark);
 console.log(isEver);
+console.log('тест гита с накаченной нодой')
 //Сколько двоечников? Есть ли отличники/двоечники? Средний балл? являются ли все двоечниками?
