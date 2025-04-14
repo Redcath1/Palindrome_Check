905d2dd9168f5c5bd40e8d1dbc35f30550599739
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-14 16:03:33 +0500

    Update Massive_Sort.js
    Completed With Reduse

--- Massive_Sort.js
+++ Massive_Sort.js
@@ -6,14 +6,16 @@ const students = [
   { id: 5, name: "Alex", mark: 2 },
 ];
 const poorsQuantity = students.filter((students) => students.mark === 2).length;
-const fivers = students.some(students => students.mark === 5);
-const massiveMarks = students.map(students => students.mark);
-const sum = students.reduce()
+const fivers = students.some((students) => students.mark === 5);
+const massiveMarks = students.map((students) => students.mark);
+const sum = massiveMarks.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
 const averageMark = sum / massiveMarks.length;
 
 console.log("количество двоечников", poorsQuantity);
 console.log("есть ли отличники", fivers);
-console.log('средняя оценка', averageMark)
-
+console.log("средняя оценка", averageMark);
 
 //Сколько двоечников? Есть ли отличники/двоечники? Средний балл?
