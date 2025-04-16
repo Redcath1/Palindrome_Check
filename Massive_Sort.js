1b891814c8fb817d65a2a431e71011952cfd8fa6
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-16 21:21:13 +0500

    refreshed

--- Massive_Sort.js
+++ Massive_Sort.js
@@ -2,7 +2,7 @@ const students = [
   { id: 1, name: "John", mark: 5 },
   { id: 2, name: "Peter", mark: 4 },
   { id: 3, name: "Vincent", mark: 3 },
-  { id: 4, name: "Sebastien", mark: 5 },
+  { id: 4, name: "Sebastien", mark: 5},
   { id: 5, name: "Alex", mark: 2 },
 ];
 const poorsQuantity = students.filter((students) => students.mark === 2).length;
@@ -10,12 +10,11 @@ const fiversCount = students.some((students) => students.mark === 5);
 const marksArray = students.map((students) => students.mark);
 const sum = students.reduce((acc, currentValue) => acc + currentValue.mark, 0);
 const averageMark = sum / marksArray.length;
-const isEver = students.every(students => students.mark === 2);
+const isEver = students.every((students) => students.mark === 2);
 function allHaveMark(students, mark) {
   for (let i = 0; i < students.length; i++) {
-    if (students[i].mark !== 2)
-    {
-      return false
+    if (students[i].mark !== 2) {
+      return false;
     }
   }
   return true;
