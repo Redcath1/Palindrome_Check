3b2603c6137d9af7efb5fe998f3bbcf90d51fac5
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-26 23:11:50 +0500

    yet another script

---
+++ figures.js
@@ -0,0 +1,23 @@
+function triangle(sym, size) {
+  let result = "";
+  for (let i = 0; i < size; i++) {
+    for (let x = 0; x <= i; x++) {
+      result += sym;
+    }
+    result += "\n";
+  }
+  return result;
+}
+console.log(triangle("*", 5));
+function isoscalesTriangle(sym, size) {
+    let result = ""
+    for (let i = 0; i < size; i++) {
+        for (let x = 0; x <= i; x++) {
+            for (let k = 0; k < array.length; k++) {
+                const element = array[k];
+                
+            };
+            
+        }
+        
+    }
