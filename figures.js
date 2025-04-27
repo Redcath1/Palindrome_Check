0ffdb9349d71ca30b08dbaaba9fc702706d693cb
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-27 23:03:21 +0500

    added empty triangle fuction

--- figures.js
+++ figures.js
@@ -8,16 +8,20 @@ function triangle(sym, size) {
   }
   return result;
 }
-console.log(triangle("*", 5));
-function isoscalesTriangle(sym, size) {
-    let result = ""
-    for (let i = 0; i < size; i++) {
-        for (let x = 0; x <= i; x++) {
-            for (let k = 0; k < array.length; k++) {
-                const element = array[k];
-                
-            };
-            
-        }
-        
+// пустой внутри треугольник
+function triangle(sym, size) {
+  let result = "";
+  for (let i = 0; i < size; i++) {
+    for (let x = 0; x <= i; x++) {
+      if (i === 0 || i === size - 1 || x === 0 || x === i) {
+        result += sym;
+      } else {
+        result += " ";
+      }
     }
+    result += "\n";
+  }
+  return result;
+}
+
+console.log(triangle("*", 5));
