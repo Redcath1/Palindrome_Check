2ccc2e910ef9bfb5c25d735c959118703b2f50bd
Redcath1 <kovtunenko-aleksei@yandex.ru>
2025-04-29 14:34:52 +0500

    optimized triangle
    собираем строки от центра +-i

--- figures.js
+++ figures.js
@@ -1,4 +1,4 @@
-function triangle(sym, size) {
+/*function triangle(sym, size) {
   let result = "";
   for (let i = 0; i < size; i++) {
     for (let k = 0; k <= i; k++) {
@@ -23,5 +23,42 @@ function triangle(sym, size) {
   }
   return result;
 }
+console.log(triangle("*", 5));
+
+//пустой внутри равнобедренный треугольник
+function triangle(sym, size) {
+  let result = "";
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size - i - 1; j++) {
+      result += " ";
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+      if (i === 0 || i === size - 1 || k === 0 || k === 2 * i) {
+        result += sym;
+      } else {
+        result += " ";
+      }
+    }
+    result += "\n";
+  }
+  return result;
+}
 
 console.log(triangle("*", 5));
+*/
+//оптимизированная версия
+
+function triangle(sym, size) {
+  let result = "";
+  const center = size - 1;
+  for (let i = 0; i < size; i++) {
+    const left = center - i;
+    const right = center + i;
+    for (let k = 0; k <= right; k++) {
+      result += k === left || k === right || i === size - 1 ? sym : " ";
+    }
+    result += "\n";
+  }
+  return result;
+}
+console.log(triangle("#", 5));
